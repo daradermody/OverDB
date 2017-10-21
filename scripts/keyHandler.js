451c161b2f32396cbba3d2bd16d80f111479b2bf
LauraMJ <lauramjphelan@gmail.com>
2017-10-21 21:21:39 +0100

    Changed list items again, fixed jslint error

--- scripts/keyHandler.js
+++ scripts/keyHandler.js
@@ -1,6 +1,6 @@
 function handleKeyPress(e) {
     var key = e.keyCode || e.which;
-    if (key == 13) {
+    if (key === 13) {
         getFilmography(document.getElementById('searchField').value);
         document.getElementById('searchField').value = "Search";
     }

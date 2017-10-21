c930a34940a1a41cdda8eb7b73e1f580334d12b9
Dara Dermody <daradermody@gmail.com>
2017-10-21 21:59:20 +0100

    Now displays fancy cards for films. Also added lint config files, and fixed some JSlint and ESlint errors/warnings

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,8 @@
-function getFilmography(actor) {
-    "use strict";
+/*jslint devel: true, browser: true */
+/*global $ */
+"use strict";
 
+function getFilmography(actor) {
     var pageId = getIdOfPage(actor);
     console.debug("ID of page: " + pageId);
 
@@ -9,29 +11,42 @@ function getFilmography(actor) {
 
     var films = getFilmsInSection(pageId, sectionId);
     console.debug("Films: " + films);
-    document.getElementById("mainContentArea").innerHTML = films.join("<br>");
+
+    var content = "";
+    for (var i in films) {
+        content += `
+            <div class="card" style="width: 20rem;">
+              <img class="card-img-top" src="images/film-xxl.png" alt="Card image cap">
+              <div class="card-body">
+                <h4 class="card-title">` + films[i] + `</h4>
+                <p class="card-text">It's a movie about cancer.</p>
+              </div>
+            </div>
+        `;
+    }
+    document.getElementById("mainContentArea").innerHTML = content;
 }
 
 function getIdOfPage(pageName) {
-    console.debug("Getting page ID")
+    console.debug("Getting page ID");
     var response = get("https://en.wikipedia.org/w/api.php?action=query&titles=" + pageName + "&format=json", console.log);
     console.debug("Response from get for getIdOfPage: " + JSON.stringify(response, null, 4));
     return Object.keys(response.query.pages)[0];
 }
 
 function getSectionIdOfPage(pageId, sectionName) {
-    console.debug("Getting section IDs")
-    var response = get("https://en.wikipedia.org/w/api.php?action=parse&pageid=" + pageId + "&prop=sections&format=json")
+    console.debug("Getting section IDs");
+    var response = get("https://en.wikipedia.org/w/api.php?action=parse&pageid=" + pageId + "&prop=sections&format=json");
     console.debug("Response from get for getSectionIdOfPage: " + JSON.stringify(response, null, 4));
-    return response.parse.sections.find(function(element) {return element.line == sectionName}).index
+    return response.parse.sections.find(function(element) {return element.line === sectionName; }).index;
 }
 
-function getFilmsInSection(pageId, sectionId){
-    var response = get("https://en.wikipedia.org/w/api.php?action=parse&pageid=" + pageId + "&section=" + sectionId + "&prop=links&format=json")
+function getFilmsInSection(pageId, sectionId) {
+    var response = get("https://en.wikipedia.org/w/api.php?action=parse&pageid=" + pageId + "&section=" + sectionId + "&prop=links&format=json");
     console.debug("Response from get for getFilmsInSection: " + JSON.stringify(response, null, 4));
     var films = [];
     console.debug(response.parse.links.values);
-    for (i in response.parse.links) {
+    for (var i in response.parse.links) {
         films.push(response.parse.links[i]["*"]);
     }
     return films
@@ -41,7 +56,7 @@ function getFilmsInSection(pageId, sectionId){
 }
 
 function get(url) {
-    var url = 'https://cors-anywhere.herokuapp.com/' + url;
+    url = 'https://cors-anywhere.herokuapp.com/' + url;
 
     var response = $.ajax({
         "async": false,

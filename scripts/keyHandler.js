5a1a0b3444366fed1a9f12f08cd8fcac0aa93877
LauraMJ <lauramjphelan@gmail.com>
2017-10-21 19:33:28 +0100

    Added bootstrap example, Added test homepage, removed wiping of search field, added example css,

--- scripts/keyHandler.js
+++ scripts/keyHandler.js
@@ -2,6 +2,5 @@ function handleKeyPress(e) {
     var key = e.keyCode || e.which;
     if (key == 13) {
         getFilmography(document.getElementById('searchField').value);
-        document.getElementById('searchField').value = "";
     }
 }

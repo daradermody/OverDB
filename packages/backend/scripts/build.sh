67f2231cbb55b8d22fb79fa4ce7ef03cdf27e435
Dara Dermody <dara.dermody@siren.io>
2022-10-04 22:45:35 +0100

    Update build

--- packages/backend/scripts/build.sh
+++ packages/backend/scripts/build.sh
@@ -1,7 +1,7 @@
 rm -rf build/*
 mkdir -p build/unbundled
 yarn graphql-type-gen
-yarn esbuild --bundle --platform=node src/index.ts --define:process.env.NODE_ENV=\"production\" --external:canvas --external:./xhr-sync-worker.js > build/unbundled/backend.js
+yarn esbuild --bundle --platform=node src/index.ts --define:process.env.NODE_ENV=\"production\" --external:canvas --external:./xhr-sync-worker.js > build/unbundled/overdb-backend.js
 touch build/unbundled/xhr-sync-worker.js
 cp src/schema.graphql build/unbundled
-yarn nexe --input build/unbundled/backend.js --output build/backend --target linux-x64-14.15.3 --resource build/unbundled/schema.graphql --resource build/unbundled/xhr-sync-worker.js
+yarn nexe --input build/unbundled/overdb-backend.js --output build/overdb-backend --target linux-x64-14.15.3 --resource build/unbundled/schema.graphql --resource build/unbundled/xhr-sync-worker.js

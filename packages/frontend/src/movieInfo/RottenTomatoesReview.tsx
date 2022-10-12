3e76a5e537eca7787aede7175394dade85dc79c1
Dara Dermody <daradermody@gmail.com>
2022-10-12 16:04:39 +0100

    Make RT score a little clearer

--- packages/frontend/src/movieInfo/RottenTomatoesReview.tsx
+++ packages/frontend/src/movieInfo/RottenTomatoesReview.tsx
@@ -1,7 +1,8 @@
-import {gql} from '@apollo/client';
-import {Skeleton, Typography} from '@mui/material';
+import { gql } from '@apollo/client'
+import styled from '@emotion/styled'
+import { Skeleton, Typography } from '@mui/material'
 import * as React from 'react'
-import {Movie, Tomatometer, TomatometerState, useGetRottenTomatoesScoreQuery} from '../../types/graphql';
+import { Movie, Tomatometer, TomatometerState, useGetRottenTomatoesScoreQuery } from '../../types/graphql'
 import ApiErrorMessage from '../shared/ApiErrorMessage'
 
 export default function RottenTomatoesReview({id}: { id: Movie['id'] }) {
@@ -30,9 +31,9 @@ export default function RottenTomatoesReview({id}: { id: Movie['id'] }) {
     <div style={{display: 'flex', alignItems: 'center', gap: 20}}>
       <a href={data.movie.tomatometer.link} target="_blank" style={{position: 'relative', color: 'inherit'}}>
         <img width="75px" src={TomatometerIcons[state]} alt={state}/>
-        <Typography variant="h2" sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', margin: '0'}}>
+        <StyledScoreText variant="h2">
           {score}%
-        </Typography>
+        </StyledScoreText>
       </a>
       <Typography variant="body1" dangerouslySetInnerHTML={{__html: consensus || 'No critics consensus'}}/>
     </div>
@@ -44,6 +45,15 @@ export const TomatometerIcons: Record<Tomatometer['state'], string> = {
   [TomatometerState.Fresh]: 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/tomatometer/tomatometer-fresh.149b5e8adc3.svg',
   [TomatometerState.Rotten]: 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/tomatometer/tomatometer-rotten.f1ef4f02ce3.svg'
 }
+
+const StyledScoreText = styled(Typography)`
+  text-shadow: 0 0 4px black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0;
+`
 
 gql`
   query GetRottenTomatoesScore($id: ID!) {
